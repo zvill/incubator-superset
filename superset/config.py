54fe81eaf81da5b07c8e9d55fedcb5a1e77275ab
zhangyw <happy491218@vip.qq.com>
2019-04-15 09:02:10 +0800

    fix number format

--- superset/config.py
+++ superset/config.py
@@ -175,21 +175,25 @@ PUBLIC_ROLE_LIKE_GAMMA = False
 # Babel config for translations
 # ---------------------------------------------------
 # Setup default language
-BABEL_DEFAULT_LOCALE = 'en'
+BABEL_DEFAULT_LOCALE = 'zh'
 # Your application default translation path
 BABEL_DEFAULT_FOLDER = 'superset/translations'
 # The allowed translation for you app
+# LANGUAGES = {
+#     'zh': {'flag': 'cn', 'name': 'Chinese'},
+#     'en': {'flag': 'us', 'name': 'English'},
+#     'it': {'flag': 'it', 'name': 'Italian'},
+#     'fr': {'flag': 'fr', 'name': 'French'},
+#     'ja': {'flag': 'jp', 'name': 'Japanese'},
+#     'de': {'flag': 'de', 'name': 'German'},
+#     'pt': {'flag': 'pt', 'name': 'Portuguese'},
+#     'pt_BR': {'flag': 'br', 'name': 'Brazilian Portuguese'},
+#     'ru': {'flag': 'ru', 'name': 'Russian'},
+#     'ko': {'flag': 'kr', 'name': 'Korean'},
+# }
 LANGUAGES = {
-    'en': {'flag': 'us', 'name': 'English'},
-    'it': {'flag': 'it', 'name': 'Italian'},
-    'fr': {'flag': 'fr', 'name': 'French'},
     'zh': {'flag': 'cn', 'name': 'Chinese'},
-    'ja': {'flag': 'jp', 'name': 'Japanese'},
-    'de': {'flag': 'de', 'name': 'German'},
-    'pt': {'flag': 'pt', 'name': 'Portuguese'},
-    'pt_BR': {'flag': 'br', 'name': 'Brazilian Portuguese'},
-    'ru': {'flag': 'ru', 'name': 'Russian'},
-    'ko': {'flag': 'kr', 'name': 'Korean'},
+    'en': {'flag': 'us', 'name': 'English'}
 }
 
 # ---------------------------------------------------
