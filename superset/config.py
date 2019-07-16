2da6b53eace69a00db4aa6f7354b9c980875dfdb
zhangyw <zhangyw@133.cn>
2019-07-16 11:11:16 +0800

    reset config.py

--- superset/config.py
+++ superset/config.py
@@ -183,15 +183,15 @@ BABEL_DEFAULT_FOLDER = "superset/translations"
 # The allowed translation for you app
 LANGUAGES = {
     "en": {"flag": "us", "name": "English"},
-    # "it": {"flag": "it", "name": "Italian"},
-    # "fr": {"flag": "fr", "name": "French"},
+    "it": {"flag": "it", "name": "Italian"},
+    "fr": {"flag": "fr", "name": "French"},
     "zh": {"flag": "cn", "name": "Chinese"},
-    # "ja": {"flag": "jp", "name": "Japanese"},
-    # "de": {"flag": "de", "name": "German"},
-    # "pt": {"flag": "pt", "name": "Portuguese"},
-    # "pt_BR": {"flag": "br", "name": "Brazilian Portuguese"},
-    # "ru": {"flag": "ru", "name": "Russian"},
-    # "ko": {"flag": "kr", "name": "Korean"},
+    "ja": {"flag": "jp", "name": "Japanese"},
+    "de": {"flag": "de", "name": "German"},
+    "pt": {"flag": "pt", "name": "Portuguese"},
+    "pt_BR": {"flag": "br", "name": "Brazilian Portuguese"},
+    "ru": {"flag": "ru", "name": "Russian"},
+    "ko": {"flag": "kr", "name": "Korean"},
 }
 
 # ---------------------------------------------------
